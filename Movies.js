f6f3bf4a8298e5e595252f0dd3fabddf38d73b03
Fsaz8 <fritziesaz13@gmail.com>
2025-01-14 19:22:40 +0300

    Update Movies.js

---
+++ Movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <p>This page will be developed in Week 4.</p>
+    </div>
+  );
+}
+
+export default Movies;
