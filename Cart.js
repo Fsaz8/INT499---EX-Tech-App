dd2de2021f3a96afdecd60736295ce5bdf44d3a0
Fsaz8 <fritziesaz13@gmail.com>
2025-01-14 19:21:25 +0300

    Update Cart.js

---
+++ Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart Page</h1>
+      <p>This page will be developed in Week 4.</p>
+    </div>
+  );
+}
+
+export default Cart;
